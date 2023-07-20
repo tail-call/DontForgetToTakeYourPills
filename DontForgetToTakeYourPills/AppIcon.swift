0915230c8cb36842c90e67036fb48c4d4f45534a
Maria Zaitseva <maria@tail-call.ru>
2023-07-20 17:26:31 +0700

    Cuter app icon view

--- DontForgetToTakeYourPills/AppIcon.swift
+++ DontForgetToTakeYourPills/AppIcon.swift
@@ -11,5 +11,12 @@ struct AppIcon: View {
     var body: some View {
         Image(uiImage: Asset.appIcon)
             .resizable(resizingMode: .stretch)
+            .clipShape(
+                RoundedRectangle(
+                    cornerSize: CGSize(width: 20, height: 0),
+                    style: .continuous
+                )
+            )
+            .shadow(radius: 2, x: 2, y: 2)
     }
 }
