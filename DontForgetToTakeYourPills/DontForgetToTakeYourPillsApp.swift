d55452044c2e85a0e8f3df6bbb4fceab970d7e1b
Maria Zaitseva <maria@tail-call.ru>
2023-07-20 16:23:00 +0700

    Initial Commit

---
+++ DontForgetToTakeYourPills/DontForgetToTakeYourPillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontForgetToTakeYourPillsApp.swift
+//  DontForgetToTakeYourPills
+//
+//  Created by Maria on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DontForgetToTakeYourPillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
